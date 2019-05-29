0ea589b0394f33dd9270d8bf24723dd415180923
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-05-29 10:27:49 +0300

    defaulted to port 3001

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.use(express.static('static'));
-app.listen(3000, function () {
-console.log('App started on port 3000');
+app.listen(3001, function () {
+    console.log('App started on port 3000');
 });
