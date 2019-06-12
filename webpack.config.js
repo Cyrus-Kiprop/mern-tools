b6d4b35f96ec2af1cd8efa612ed7e3765bf6b630
Cyrus-Kiprop <cyruskiprop254@gmail.com>
2019-06-12 12:06:07 +0300

    added HMR configs, devtools and source-maps

--- webpack.config.js
+++ webpack.config.js
@@ -3,12 +3,19 @@ const path = require('path');
 //const webpack = require('webpack')
 
 const config = {
-    mode: 'production',
-    entry: './src/App.jsx',
+    // this is a preprocessor that maps bundled code into its initial sourced code and other minified files
+    devtool: 'source-map',
+    mode: 'development',
+    // to accept HMR middlewares entry points will be tweaked to be an array or object
+    entry: {
+        app: ['./src/App.jsx']
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname + 'static'),
         filename: 'app.bundle.js'
     },
+    // this plugin is for HMR middlewares
+    plugins: [],
     module: {
         rules: [{
             test: /\.(js|jsx)?$/,
@@ -16,5 +23,15 @@ const config = {
             exclude: /node_modules/
         }]
     },
+    // this one handle the webpark-dev-server port to apis
+    devServer: {
+        port: 8080,
+        contentBase: 'static',
+        proxy: {
+            '/api/*': {
+                target: 'http://localhost:3001'
+            }
+        }
+    }
 };
 module.exports = config;
